92b7fc384e115b6130188ac20d381cce4918a23c
Baekjoon Choi <choi@startlink.io>
2016-10-28 23:18:19 +0900

    initial commit

---
+++ 12852.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cassert>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int main(int agrc, char **agrv){
+    FILE *in = fopen(agrv[1],"r");
+    FILE *sol = fopen(agrv[2],"r");
+    FILE *out = fopen(agrv[3],"r");
+    int n;
+    fscanf(in,"%d",&n);
+    fclose(in);
+    int ans;
+    fscanf(sol,"%d",&ans);
+    fclose(sol);
+    int yourans;
+    assert(fscanf(out,"%d",&yourans)==1);
+    assert(ans==yourans);
+    int a, b;
+    assert(fscanf(out,"%d",&a)==1);
+    assert(n == a);
+    for (int i=0; i<yourans; i++) {
+        assert(fscanf(out,"%d",&b)==1);
+        assert(a-1 == b || a/2 == b || a/3 == b);
+        a=b;
+    }
+    assert(a==1);
+    assert(fscanf(out,"%d",&b)!=1);
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# BOJ 스페셜 저지
+
+아래 문제의 스페셜 저지를 만들어 주세요.
+
+스페셜 저지는 아래 템플릿를 이용해서 만들면 됩니다.
+
+```
+#include <cstdio>
+#include <cassert>
+using namespace std;
+int main(int agrc, char **agrv){
+    FILE *in = fopen(agrv[1],"r");
+    FILE *sol = fopen(agrv[2],"r");
+    FILE *out = fopen(agrv[3],"r");
+    return 0;
+}
+```
+
+Pull Request를 이용해서 보내주세요.
+
+첨부되어 있는 12852.cpp는 [12852번: 1로 만들기 2](https://www.acmicpc.net/problem/12852)의 채점 소스입니다.
